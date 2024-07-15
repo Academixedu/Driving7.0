29c4027e01e99b89b05ed72a48a673f06495ddad
saikumar34892 <saikumarmatta3821@gmail.com>
2024-07-15 13:47:49 +0530

    sai commit

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 
 public class DrivingLicense {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int id;
-        String name;
-        int age;
-        String address;
-// Remaining Code Here
+        Scanner ion=new Scanner(System.in);
+        System.out.println("enter id");
+         int id = ion.nextInt();  
+        System.out.println("enter name:");
+        String name = ion.next();
+        System.out.println("enter age");
+        int age = ion.nextInt();
+        System.out.println("enter address");
+        String address = ion.next();
+        if(age>=18){
+            System.out.println("eligible to drive"+" "+"\n id:"+" "+id +"\n name:"+" "+name +" "+"\n age:"+" "+age +"\n address:"+" "+address);
+        }
+        else{
+            System.out.println(" not eligible to drive"+" "+"\n id:"+" "+id +"\n name:"+" "+name +" "+"\n age:"+" "+age +"\n address:"+" "+address);
+        }
     }
 }
