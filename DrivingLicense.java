e272d3713d99c29969e2911e9c0d531f5ad43d50
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-07-15 13:19:37 +0530

    first commit

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -7,6 +7,22 @@ public class DrivingLicense {
         String name;
         int age;
         String address;
-// Remaining Code Here
+        System.out.print("Enter your name: ");
+        name = in.nextLine();
+        System.out.print("Enter your Id: ");
+        id = in.nextInt();
+        System.out.print("Enter your age: ");
+        age = in.nextInt();
+        System.out.print("Enter your address: ");
+        address = in.next();
+
+        if(age>=18){
+            System.out.println("This candidate is Eligible for DrivingLicense");
+            System.out.println("Name: "+name+"\n Id: "+id+"\n Age: "+age+"\n Address: "+address);
+        }
+        else{
+            System.out.println("This candidate is not Eligible for DrivingLicense");
+            System.out.println("Name: "+name+"\n Id: "+id+"\n Age: "+age+"\n Address: "+address);
+        }
     }
 }
