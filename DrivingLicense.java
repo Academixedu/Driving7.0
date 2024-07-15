9bbce548dad00f5d4e9bce237f701159aa3be182
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-15 13:28:00 +0530

    add file

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
 public class DrivingLicense {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int id;
-        String name;
-        int age;
-        String address;
-// Remaining Code Here
+        System.out.println("enter id number");
+        int id =in.nextInt();
+        Scanner i=new Scanner(System.in);
+        System.out.println("enter name");
+        String name=i.nextLine();
+        System.out.println("enter age");
+        int age=in.nextInt();
+        System.out.println("enter address");
+        String address=i.nextLine();
+    
+    if(age>=18){
+        System.out.println("ID:"+id);
+        System.out.println("NAME:" +name);
+        System.out.println("AGE:"+age);
+        System.out.println("ADDRESS:"+address);
+        System.out.println("you are eligble for  Drivinglicence");
     }
+    else{
+        System.out.println(" you are not eligble for licence");
+    }
+}
 }
