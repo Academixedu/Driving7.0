7369d673172697d7c965e4621f63978fd51739d4
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-15 13:37:03 +0530

    mani

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -7,6 +7,39 @@ public class DrivingLicense {
         String name;
         int age;
         String address;
-// Remaining Code Here
+        
+        System.out.println("enter name");
+        name = in.nextLine();
+
+        System.out.println("enter id ");
+        id = in.nextInt();
+
+        
+
+        System.out.println("enter age");
+        age = in.nextInt();
+
+        System.out.println("enter address");
+        address = in.next();
+
+        System.out.println("");
+        
+        if(age < 18)
+        {
+            System.out.println("Not Eligible for Driving License\n\n Age is less than 18\n age is "+age+"\n");
+        }
+        else{
+            System.out.println("Eligible for Driving License\n");
+        }
+
+        System.out.println("DETAILS OF CANDIDATE");
+        System.out.println("-----------------------");
+
+        System.out.println("ID = "+id+"\nNAME = "+name+" \nAGE = "+age+" \nADDRESS = "+address);
+
+        
+
+
+
     }
 }
