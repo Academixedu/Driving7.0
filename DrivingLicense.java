efcc654a343123031eb2d69c3c2080e7dba3914e
kavitha-guntru <kavithaguntru60@gmail.com>
2024-07-16 12:46:00 +0530

    kavitha

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -2,11 +2,37 @@ import java.util.Scanner;
 
 public class DrivingLicense {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+        Scanner a =new Scanner(System.in);
+        
         int id;
-        String name;
+        String Name;
         int age;
         String address;
+
 // Remaining Code Here
+
+        System.out.println("enter name:");
+        Name = a.nextLine();
+
+        System.out.println("enter id");
+        id = a.nextInt();  
+
+        System.out.println("enter age");
+        age = a.nextInt();
+        
+        System.out.println("enter address");
+        address = a.next();
+        
+        if(age >= 18)
+        {
+            System.out.println("eligible to driving licences ");
+        }
+        else
+        {
+            System.out.println("Not eligible ");
+        }
+
     }
 }
+
+
